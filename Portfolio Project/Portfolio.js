fae1c7f82abfc994e798747b29845e5f4480202c
baghelyogendra <124449213+baghelyogendra@users.noreply.github.com>
2023-04-08 09:00:36 +0530

    Add files via upload

---
+++ Portfolio Project/Portfolio.js
@@ -0,0 +1,16 @@
+/*let btnopen = document.querySelector ('io');
+let input = document.querySelector ('input');
+
+btnopen.addEventListener('click', () => {
+    window.open (input.value, 'https://github.com/baghelyogendra')
+})
+function show() {
+    document.querySelector('.menu').classList
+    .toggle('open')
+    document.querySelector('.hamburger').classList
+    .toggle('active')
+}*/
+function onclickmenu(){
+    document.getElementsByClassName('side').classList.toggle("icon")
+    document.getElementsByClassName('menu').classList.toggle("change")
+}
